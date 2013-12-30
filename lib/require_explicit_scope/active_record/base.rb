c4ea06e49cda75a18d1c89a871145cecd1e05d7b
Andrew Geweke <andrew@geweke.org>
2013-12-30 10:39:35 -0500

    Remove unused code.

--- lib/require_explicit_scope/active_record/base.rb
+++ lib/require_explicit_scope/active_record/base.rb
@@ -7,30 +7,12 @@ module RequireExplicitScope
 
       module ClassMethods
         def require_explicit_scope
-          # self.default_scopes = [ ]
-
-          # default_scope { raise("You must explicitly specify a scope, yo") }
           @_explicit_scope_required = true
         end
 
         def explicit_scope_required?
           !! @_explicit_scope_required
         end
-
-        # def all
-        #   if current_scope
-        #     current_scope.clone
-        #   else
-        #     scope = relation
-        #     scope.default_scoped = true
-        #     scope
-        #   end
-        # end
-
-        # def all(*args, &block)
-        #   $stderr.puts "ALL CALLED WITH: #{args.inspect}; current_scope: #{current_scope.inspect}"
-        #   super(*args, &block)
-        # end
 
         def scope(name, body, *more)
           if more && more[-1] && more[-1].kind_of?(Hash)
